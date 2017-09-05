685d2fdefdc3f5db3cc518975f682086c32d3ce6
kongdejian <dejian.kdj@autonavi.com>
2017-09-05 21:30:23 +0800

    init

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,17 @@
 /**
  * Created by kongxiaojian on 2017/9/5.
  */
+var Vue = require('./lib/vue');
+var index = {
+    vue:null,
+    init:function () {
+        this.vue = new Vue({
+            data:{},
+            el:'#app',
+            components:{
+                paint:require('../vue/app.vue')
+            }
+        });
+    },
+};
+index.init();
